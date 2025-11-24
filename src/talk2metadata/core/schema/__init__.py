3c0ee0bb10fa6a85961da18e7d8aed9de5d63ac2
PascalSun <pascalsun23@gmail.com>
2025-11-24 21:48:05 +0800

    refactor code for schema detection

--- src/talk2metadata/core/schema/__init__.py
+++ src/talk2metadata/core/schema/__init__.py
@@ -1,16 +1,12 @@
 """Schema detection and metadata management."""
 
-from talk2metadata.core.schema.schema import (
-    ForeignKey,
-    SchemaDetector,
-    SchemaMetadata,
-    TableMetadata,
-)
+from talk2metadata.core.schema.schema import SchemaDetector, SchemaMetadata
 from talk2metadata.core.schema.schema_viz import (
     export_schema_for_review,
     generate_html_visualization,
     validate_schema,
 )
+from talk2metadata.core.schema.types import ForeignKey, TableMetadata
 
 __all__ = [
     "ForeignKey",
